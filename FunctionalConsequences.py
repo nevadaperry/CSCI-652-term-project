5e21af8df32a9aa0b7c740b81b00d029cf598361
xlea99 <52758416+xlea99@users.noreply.github.com>
2023-11-22 17:43:51 -0600

    Few fixes, reorganized some code

--- FunctionalConsequences.py
+++ FunctionalConsequences.py
@@ -7,12 +7,12 @@ This file contains functions used to help visualize some of the higher level fun
 consequences resulted from comparing SARS-COV2 to its variants.
 """
 import Setup as s
-
+maxVariantSize = 10**100
 
 # Given a list of variant dicts, this method returns a similar list ordered
 # by size. Optional parameter to omit variants smaller than a certain size.
-def sortVariantsBySize(variantList : list,omitVariantsSmallerThan : int = 0):
-    sortedVariants = [item for item in sorted(variantList, key=lambda x: x['Length'], reverse=True) if item['Length'] >= omitVariantsSmallerThan]
+def filterVariantsBySize(variantList : list,minVariantSize : int = 0,maxVariantSize : int = maxVariantSize):
+    sortedVariants = [item for item in sorted(variantList, key=lambda x: x['Length'], reverse=True) if minVariantSize <= item['Length'] <= maxVariantSize]
     return sortedVariants
 
 # Given a list of variant dicts, this method returns a similar list filtered only for
@@ -58,20 +58,17 @@ variants = []
 for _pwDictName,_pwDict in s.data["pw"].items():
     variants += _pwDict["Variants"]
 
-# Sorting variant list by size
-variants = sortVariantsBySize(variantList = variants,omitVariantsSmallerThan=0)
-
 # Filtering our variant list to only view variants that occur on the Spike gene, and within a specific subunit.
 spikeVariants = filterVariantsByUnit(variantList = variants, unit = "S",)
 
-
-spikeVariants_NTD = filterVariantsByUnit(variantList=spikeVariants,subunit="NTD")
-spikeVariants_RBD = filterVariantsByUnit(variantList=spikeVariants,subunit="RBD")
-spikeVariants_Cleavage = filterVariantsByUnit(variantList=spikeVariants,subunit="Cleavage")
-spikeVariants_FP = filterVariantsByUnit(variantList=spikeVariants,subunit="FP")
-spikeVariants_IFP = filterVariantsByUnit(variantList=spikeVariants,subunit="IFP")
-spikeVariants_HR1 = filterVariantsByUnit(variantList=spikeVariants,subunit="HR1")
-spikeVariants_HR2 = filterVariantsByUnit(variantList=spikeVariants,subunit="HR2")
-spikeVariants_TM = filterVariantsByUnit(variantList=spikeVariants,subunit="TM")
-spikeVariants_CT = filterVariantsByUnit(variantList=spikeVariants,subunit="CT")
-spikeVariants_Other = filterVariantsByUnit(variantList=spikeVariants,subunit="Other")
+# Creating lists of variants that occur on each specific subUnit of the Spike gene.
+all_spikeVariants_NTD = filterVariantsByUnit(variantList=spikeVariants,subunit="NTD")
+all_spikeVariants_RBD = filterVariantsByUnit(variantList=spikeVariants,subunit="RBD")
+all_spikeVariants_Cleavage = filterVariantsByUnit(variantList=spikeVariants,subunit="Cleavage")
+all_spikeVariants_FP = filterVariantsByUnit(variantList=spikeVariants,subunit="FP")
+all_spikeVariants_IFP = filterVariantsByUnit(variantList=spikeVariants,subunit="IFP")
+all_spikeVariants_HR1 = filterVariantsByUnit(variantList=spikeVariants,subunit="HR1")
+all_spikeVariants_HR2 = filterVariantsByUnit(variantList=spikeVariants,subunit="HR2")
+all_spikeVariants_TM = filterVariantsByUnit(variantList=spikeVariants,subunit="TM")
+all_spikeVariants_CT = filterVariantsByUnit(variantList=spikeVariants,subunit="CT")
+all_spikeVariants_Other = filterVariantsByUnit(variantList=spikeVariants,subunit="Other")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import Setup as s
+import FunctionalConsequences as fc
+import matplotlib.pyplot as plt
 
 
 # Simply display gap rate and sub rate for all maf processed files.
@@ -9,3 +10,40 @@ for key,value in b.data["pw"].items():
     print(f"Sub Rate: {value['SubstitutionRate']}\n")
 '''
 
+# Display a bar graph of the frequency of spike protein variations between a certain size range
+# across all pairwise alignments
+if(True):
+    minimumSize = 0
+    maximumSize = 1
+
+    #region === Processing ===
+    adjusted_spikeVariants_NTD = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_NTD,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_RBD = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_RBD,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_Cleavage = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_Cleavage,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_FP = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_FP,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_IFP = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_IFP,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_HR1 = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_HR1,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_HR2 = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_HR2,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_TM = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_TM,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_CT = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_CT,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    adjusted_spikeVariants_Other = fc.filterVariantsBySize(variantList = fc.all_spikeVariants_Other,minVariantSize=minimumSize,maxVariantSize=maximumSize)
+    spikeGeneSubunitCounts = {"CT" : len(adjusted_spikeVariants_CT),
+                              "Cleavage" : len(adjusted_spikeVariants_Cleavage),
+                              "FP" : len(adjusted_spikeVariants_FP),
+                              "HR1" : len(adjusted_spikeVariants_HR1),
+                              "HR2" : len(adjusted_spikeVariants_HR2),
+                              "IFP" : len(adjusted_spikeVariants_IFP),
+                              "NTD" : len(adjusted_spikeVariants_NTD),
+                              "RBD" : len(adjusted_spikeVariants_RBD),
+                              "TM" : len(adjusted_spikeVariants_TM)}
+    #endregion === Processing ===
+
+    plt.figure(figsize=(10,6))
+    plt.bar(list(spikeGeneSubunitCounts.keys()),list(spikeGeneSubunitCounts.values()),color="skyblue")
+
+    plt.xlabel('Subunit of Spike Gene')
+    plt.ylabel('Number of Variants')
+    plt.title('Distribution of SNP Variants Across Spike Protein Subunits')
+    plt.xticks(rotation=45)  # Rotate labels for better readability
+
+    plt.show()
