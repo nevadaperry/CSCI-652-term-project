9293509d129efaee5f61297dc7ba5e0cf4fc956e
Alex Somheil <alexlsomheil@gmail.com>
2023-11-22 00:53:27 +0000

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,55 @@
+import tomli
+import os
+import MafProcessing
+
+# region === Config and Pathing Setup ===
+
+with open("config.toml", "rb") as f:
+    config = tomli.load(f)
+
+# Simple class to validate and store program paths.
+class Paths:
+    def __init__(self):
+        # Helper function to create path (if needed) and test for access.
+        def createAndCheckAccess(pathString):
+            os.makedirs(pathString, exist_ok=True)
+            if(not os.access(pathString,os.R_OK | os.W_OK | os.X_OK)):
+                raise PermissionError(f"Insufficient permissions for configured directory: {pathString}")
+
+        # Root path for working program directory.
+        self.base = os.getcwd()
+        createAndCheckAccess(self.base)
+
+        # Data path for all data used in the program.
+        self.data = f"{self.base}\\data"
+        createAndCheckAccess(self.base)
+
+paths = Paths()
+
+# endregion === Config and Pathing Setup ===
+
+#region === Data Setup ===
+
+data = {"seqs" : {},
+        "pw" : {},
+        "pw-more" : {}}
+
+# Reading all seq files.
+for filename in os.listdir(f"{paths.data}\\seqs"):
+    with open(f"{paths.data}\\seqs\\{filename}", "r") as f:
+        seq = ""
+        for line in f:
+            if(line.startswith(">")):
+                # Skip header/comment lines
+                continue
+            else:
+                seq += line.strip()
+        data["seqs"][filename] = seq
+
+# Reading all maf files.
+for filename in os.listdir(f"{paths.data}\\pw"):
+    data["pw"][filename.split(".sing.maf")[0]] = MafProcessing.processFullMafFile(f"{paths.data}\\pw\\{filename}")
+for filename in os.listdir(f"{paths.data}\\pw-more"):
+    data["pw-more"][filename.split(".sing.maf")[0]] = MafProcessing.processFullMafFile(f"{paths.data}\\pw-more\\{filename}")
+
+#endregion === Data Setup ===
