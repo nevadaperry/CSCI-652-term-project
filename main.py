9010f0a92c7ac9358e963be41c659b446e10d3d9
xlea99 <52758416+xlea99@users.noreply.github.com>
2023-12-05 05:20:35 -0600

    fixes fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import FunctionalConsequences as fc
 import matplotlib.pyplot as plt
 
-
 # Simply display gap rate and sub rate for all maf processed files.
 '''
 for key,value in b.data["pw"].items():
@@ -12,8 +11,7 @@ for key,value in b.data["pw"].items():
 
 # Display a bar graph of the frequency of spike protein variations between a certain size range
 # across all pairwise alignments
-spikeVariantsFrequencyBySubunit = False
-if(spikeVariantsFrequencyBySubunit):
+def spikeVariantsFrequencyBySubunit():
     minimumSize = 0
     maximumSize = 1
 
@@ -49,42 +47,88 @@ if(spikeVariantsFrequencyBySubunit):
 
     plt.show()
 
-
-spikeVariantsFrequencyByVariant = True
-if(spikeVariantsFrequencyByVariant):
-    minimumSize = 0
-    maximumSize = 1
-
+# Display a bar graph of the frequency of spike protein mutations by each VOC.
+def spikeMutationsFrequencyByVariant():
     #region === Processing ===
     spikeGeneVariantCounts = {"Alpha" : len(fc.all_spikeVariants_alpha),
                               "Beta" : len(fc.all_spikeVariants_beta),
                               "Delta" : len(fc.all_spikeVariants_delta),
-                              "Epsilon" : len(fc.all_spikeVariants_epsilon),
-                              "Eta" : len(fc.all_spikeVariants_eta),
                               "Gamma" : len(fc.all_spikeVariants_gamma),
-                              "Iota" : len(fc.all_spikeVariants_iota),
-                              "Kappa" : len(fc.all_spikeVariants_kappa),
-                              "Lambda" : len(fc.all_spikeVariants_lambda),
-                              "Mu" : len(fc.all_spikeVariants_mu),
-                              "Omicron" : len(fc.all_spikeVariants_omicron),
-                              "OmicronBA1" : len(fc.all_spikeVariants_omicronBA1),
-                              "OmicronBA2" : len(fc.all_spikeVariants_omicronBA2),
-                              "OmicronBA3" : len(fc.all_spikeVariants_omicronBA3),
-                              "OmicronBA4" : len(fc.all_spikeVariants_omicronBA4),
-                              "OmicronBA5" : len(fc.all_spikeVariants_omicronBA5),
-                              "OmicronBQ1" : len(fc.all_spikeVariants_omicronBQ1),
-                              "OmicronXAK" : len(fc.all_spikeVariants_omicronXAK),
-                              "OmicronXBB1" : len(fc.all_spikeVariants_omicronXBB1),
-                              "Theta" : len(fc.all_spikeVariants_theta),
-                              "Zeta" : len(fc.all_spikeVariants_zeta)}
+                              "OmicronBA1" : len(fc.all_spikeVariants_omicronBA1)}
+
     #endregion === Processing ===
 
     plt.figure(figsize=(10,6))
-    plt.plot(list(spikeGeneVariantCounts.keys()),list(spikeGeneVariantCounts.values()),color="skyblue")
+    bars = plt.bar(list(spikeGeneVariantCounts.keys()),list(spikeGeneVariantCounts.values()),color="skyblue")
 
-    plt.xlabel('Subunit of Spike Gene')
-    plt.ylabel('Number of Variants')
-    plt.title('Distribution of SNP Variants Across Spike Protein Subunits')
+    plt.xlabel('Variant of Interest')
+    plt.ylabel('Number of Spike Gene Mutations')
+    plt.title('Frequency of Spike Gene Mutations by Variant of Interest')
     plt.xticks(rotation=45)  # Rotate labels for better readability
 
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, int(yval), ha='center', va='bottom')
+
     plt.show()
+
+# Display a line plot showing the transmissibility (R) rate of each VOC.
+def transmissibilityByVariant():
+
+    # region === Processing ===
+    spikeGeneVariantTransmissibilities = {"Alpha" : fc.transmissibility["alpha"]["AvgR"],
+                              "Beta" : fc.transmissibility["beta"]["AvgR"],
+                              "Delta" : fc.transmissibility["delta"]["AvgR"],
+                              "Gamma" : fc.transmissibility["gamma"]["AvgR"],
+                              "OmicronBA1" : fc.transmissibility["omicron"]["AvgR"]}
+    # endregion === Processing ===
+
+    plt.figure(figsize=(10, 6))
+    linePlot = plt.plot(list(spikeGeneVariantTransmissibilities.keys()), list(spikeGeneVariantTransmissibilities.values()), color="skyblue")
+
+    plt.xlabel('Variant of Interest')
+    plt.ylabel('Transmissibility (R) Factor')
+    plt.title('Transmissibility Rates of Studied Variants of Interest')
+    plt.xticks(rotation=45)  # Rotate labels for better readability
+
+    # Adding the text on each point
+    x_data = list(spikeGeneVariantTransmissibilities.keys())
+    y_data = list(spikeGeneVariantTransmissibilities.values())
+    for i, txt in enumerate(y_data):
+        plt.text(x_data[i], y_data[i], f'{txt:.2f}', ha='center', va='bottom')
+
+    plt.show()
+
+# Display a line plot showing the transmissibility (R) rate of each VOC.
+def indelTypesByVariant(variantName):
+
+    # region === Processing ===
+    numMutationsByRegion = {
+        "NTD": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_NTD,compareGenomeName=variantName)),
+        "RBD": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_RBD,compareGenomeName=variantName)),
+        "Cleavage": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_Cleavage,compareGenomeName=variantName)),
+        "FP": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_FP,compareGenomeName=variantName)),
+        "IFP": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_IFP,compareGenomeName=variantName)),
+        "HR1": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_HR1,compareGenomeName=variantName)),
+        "HR2": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_HR2,compareGenomeName=variantName)),
+        "TM": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_TM,compareGenomeName=variantName)),
+        "CT": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_CT,compareGenomeName=variantName)),
+        "Other": len(fc.filterMutationsByComparedGenome(mutationList=fc.sub_spikeMutations_Other,compareGenomeName=variantName)),
+    }
+    # endregion === Processing ===
+
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(list(numMutationsByRegion.keys()), list(numMutationsByRegion.values()), color="skyblue")
+
+    plt.xlabel('Subregion of Genome')
+    plt.ylabel('Frequency of Mutations')
+    plt.title(f'Frequency of Mutations by Subregion for the {variantName.capitalize()} VOC')
+    plt.xticks(rotation=45)  # Rotate labels for better readability
+
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, yval, int(yval), ha='center', va='bottom')
+
+    plt.show()
+
+
